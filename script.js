67ea8adefa87ec56b382b11f643ee02869c19a00
CanixAc <alejandrocanoosp118@gmail.com>
2025-10-17 12:19:19 -0500

    cambio completo

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-document.addEventListener("DOMContentLoaded", function(){
-      const navToggle = document.getElementById("navToggle");
-      const navMenu = document.getElementById("navMenu");
-      const navToggleLines = document.querySelectorAll("nav__toggle-line");
+document.addEventListener('DOMContentLoaded', function() {
+    const navToggle = document.getElementById('navtoggle');
+    const navMenu = document.getElementById('navmenu');
+    const navToggleLines = document.querySelectorAll('.nav__toggle-line');
 
-      navToggle.addEventListener("click", function(){
-      navMenu.classList.toggle("nav__menu--action");
+    navToggle.addEventListener('click', function() {
+        navMenu.classList.toggle('nav__menu--active');
 
-      navToggleLines[0].classList.toggle("nav__toggle-line--top-active");
-      navToggleLines[1].classList.toggle("nav__toggle-line--middle-active");
-      navToggleLines[2].classList.toggle("nav__toggle-line--botton-active");
-
-      })
-})
+        navToggleLines[0].classList.toggle('nav__toggle-line--top-active');
+        navToggleLines[1].classList.toggle('nav__toggle-line--middle-active');
+        navToggleLines[2].classList.toggle('nav__toggle-line--bottom-active');
+    });
+});
